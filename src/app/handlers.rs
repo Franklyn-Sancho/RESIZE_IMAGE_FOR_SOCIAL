4727f72d471c415c4773a29c8ec263fdc582a8d7
Franklyn Sancho <franklyn.hs@gmail.com>
2023-08-11 19:49:00 -0300

    fix: error web version resized image

--- src/app/handlers.rs
+++ src/app/handlers.rs
@@ -2,7 +2,7 @@
 
 use actix_files::NamedFile;
 use actix_web::{web, Error, HttpResponse};
-use futures::{StreamExt, TryStreamExt};
+use image::DynamicImage;
 use serde_derive::Deserialize;
 
 use crate::image_resizer::ImageResizer;
@@ -14,23 +14,40 @@ pub struct ResizeRequest {
     social_platform_name: String,
 }
 
+//decodifica os dados de entrada
+fn decode_input_data(input_data: &str) -> Vec<u8> {
+    base64::decode(input_data).unwrap()
+}
+
+//cria um objeto ImageResizer
+fn create_resizer(
+    input_data: Vec<u8>,
+    output_path_name: &str,
+    social_plataform_name: &str,
+) -> Option<ImageResizer> {
+    let mut output_path = output_path_name.to_string();
+    output_path.push_str(".jpg");
+    ImageResizer::new(input_data, output_path_name, social_plataform_name)
+}
+
+//redimensiona a imagem
+fn resize_image_data(input_data: &[u8], resizer: &ImageResizer) -> DynamicImage {
+    let img = image::load_from_memory(input_data).unwrap();
+    resizer.resize(&img)
+}
+
+//handlers
 pub async fn resize_image(req: web::Json<ResizeRequest>) -> Result<HttpResponse, Error> {
-    let input_data = base64::decode(&req.input_data).unwrap();
-    let mut output_path_name = req.output_path_name.clone();
-    let social_platform_name = &req.social_platform_name;
-
-    output_path_name.push_str(".jpg");
-
-    // Carregar a imagem a partir dos dados
-    let img = image::load_from_memory(&input_data).unwrap();
-
-    // Criar um objeto ImageResizer
-    let resizer = ImageResizer::new(input_data, &output_path_name, social_platform_name);
+    let input_data = decode_input_data(&req.input_data);
+    let resizer = create_resizer(
+        input_data.clone(),
+        &req.output_path_name,
+        &req.social_platform_name,
+    );
 
     if let Some(resizer) = resizer {
         // Redimensionar a imagem
-        let resized_img = resizer.resize(&img);
-
+        let resized_img = resize_image_data(&input_data, &resizer);
         // Salvar a imagem redimensionada
         resizer.save_output_image(&resized_img);
 
@@ -46,4 +63,3 @@ pub async fn download_image(req: web::Path<String>) -> Result<NamedFile, Error> 
     let filepath = format!("output/{}", filename);
     Ok(NamedFile::open(filepath)?)
 }
-
--- static/script.js
+++ static/script.js
@@ -1,39 +1,54 @@
-document.getElementById("resize-form").addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    // Ler os dados do formulário
+//ler os dados do formulário
+function readFormData() {
     let imageFile = document.getElementById("input-file").files[0];
-    console.log(`image file: ${imageFile}`)
     let outputPathName = document.getElementById("output-path").value;
     let socialPlatformName = document.getElementById("social-platform").value;
 
+    return { imageFile, outputPathName, socialPlatformName };
+}
+
+//converter o arquivo de imagem como uma string base64
+function encodeImageFile(imageFile) {
+    return new Promise((resolve) => {
+        let reader = new FileReader();
+        reader.readAsDataURL(imageFile);
+        reader.onload = () => {
+            let imageData = reader.result.split(",")[1];
+            resolve(imageData);
+        };
+    });
+}
+//enviar os dados do formulário como uma solicitação POST
+async function sendResizeRequest(data) {
+    return fetch("/resize", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    }).then((response) => response.text());
+}
+
+document.getElementById("resize-form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    // Ler os dados do formulário
+    let { imageFile, outputPathName, socialPlatformName } = readFormData();
 
     // Codificar o arquivo de imagem como uma string base64
-    let reader = new FileReader();
-    reader.readAsDataURL(imageFile);
-    reader.onload = () => {
-        console.log('Antes da definição de imageData');
-        let imageData = reader.result.split(",")[1];
-        console.log('Depois da definição de imageData');
+    let imageData = await encodeImageFile(imageFile);
+    console.log("imageData: ", imageData)
 
-        let data = {
-            input_data: imageData,
-            output_path_name: outputPathName,
-            social_platform_name: socialPlatformName,
-        };
-        console.log(data);
-
-        // Enviar os dados do formulário como uma solicitação POST com o corpo no formato application/json
-        fetch("/resize", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        })
-            .then((response) => response.text())
-            .then((text) => {
-                document.getElementById("result").textContent = text;
-            });
+    let data = {
+        input_data: imageData,
+        output_path_name: outputPathName,
+        social_platform_name: socialPlatformName,
     };
+    console.log("data: ", data)
+
+    // Enviar os dados do formulário como uma solicitação POST
+    let text = await sendResizeRequest(data);
+    console.log("text: ", text)
+
+    document.getElementById("result").textContent = text;
 });
